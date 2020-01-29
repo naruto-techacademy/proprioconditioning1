5eacd970d3983860dff746b60909692a24a612a9
naruto.tech <naruto.techacademy@gmail.com>
2020-01-29 14:12:40 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Welcome to Proprio Conditioning</h1>
-            <h3><br>日々のトレーニング強度とトレーニング時間を記録して、<br>最適なコンディショニングにつなげよう！</h3>
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                 <h1>Welcome to Proprio Conditioning</h1>
+                <h3><br>日々のトレーニング強度とトレーニング時間を記録して、<br>最適なコンディショニングにつなげよう！</h3>
              {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
         </div>
-    </div>
+    @endif
 @endsection
