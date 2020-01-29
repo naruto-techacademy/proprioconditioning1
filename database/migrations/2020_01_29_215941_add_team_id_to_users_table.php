f7852b107567d1cc14690edd3404c8935458bc35
naruto.tech <naruto.techacademy@gmail.com>
2020-01-29 13:47:47 +0000

    user registration

--- database/migrations/2020_01_29_215941_add_team_id_to_users_table.php
+++ database/migrations/2020_01_29_215941_add_team_id_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class AddTeamIdToUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,8 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
+        Schema::table('users', function (Blueprint $table) {
             $table->string('team_id');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
         });
     }
 
@@ -31,6 +25,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('team_id');
+        });
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to Proprio Conditioning</h1>
-            <h3>日々のトレーニング強度とトレーニング時間を入力して、<br>適切なコンディショニングにつなげましょう。</h3>
+            <h3><br>日々のトレーニング強度とトレーニング時間を記録して、<br>最適なコンディショニングにつなげよう！</h3>
+             {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
